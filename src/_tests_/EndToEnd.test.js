4cd51e7e11d364db3f8d1a656c1a3ecb360e2d46
Niket Shukla <niketshukla@gmail.com>
2022-06-08 08:49:49 -0400

    Implemented End-To-End testing and Acceptence testing

---
+++ src/_tests_/EndToEnd.test.js
@@ -0,0 +1,39 @@
+import puppeteer from "puppeteer";
+
+describe('show/hide an event details', () => {
+    let browser;
+    let page;
+    jest.setTimeout(60000);
+
+    beforeAll(async () => {
+      browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 250,
+        ignoreDefaultArgs: ['--disable-extensions']
+      });
+      page = await browser.newPage();
+      await page.goto('http://localhost:3000/MeetUp');
+      await page.waitForSelector('.event');
+    });
+
+    test('An event element is collapsed by default', async () => {
+        const eventDetails = await page.$('.event .extra-details');
+        expect(eventDetails).toBeNull();
+    });
+
+    test('User can expand an event to see its details', async () => {
+        await page.click('.event .details-button');
+        const eventDetails = await page.$('.event .extra-details');
+        expect(eventDetails).toBeDefined();
+    });
+
+    test('User can collapse an event to hide its details', async () => {
+        await page.click('.event .details-button');
+        const eventDetails = await page.$('.event .extra-details');
+        expect(eventDetails).toBeNull();
+    });
+
+    afterAll(() => {
+        browser.close();
+    });
+});
