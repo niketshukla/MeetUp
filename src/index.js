7836a91177db756dbd823a97a411a19f81070966
Niket Shukla <niketshukla@gmail.com>
2022-06-08 14:25:10 -0400

    Implemented Atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+atatus.config('cd4513bb3fc344918f968b5215880d73').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
