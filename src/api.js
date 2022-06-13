fc3ec1a66b9d2560c47085babf7b6e9ac9d1142d
Niket Shukla <niketshukla@gmail.com>
2022-06-13 17:27:31 -0400

    Implemented offline data

--- src/api.js
+++ src/api.js
@@ -42,6 +42,12 @@ const removeQuery = () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const data = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return data?JSON.parse(data).events:[];;
+  }
+
   const token = await getAccessToken();
 
   if (token) {
