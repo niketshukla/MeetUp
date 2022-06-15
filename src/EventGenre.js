c2659b0405fe441e2ae2a69f67809a38fca0b86e
Niket Shukla <niketshukla@gmail.com>
2022-06-15 14:13:25 -0400

    Implementing Recharts

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -24,7 +24,7 @@ const EventGenre = ({ events }) => {
                     cx='50%'
                     cy='50%'
                     labelLine={false}
-                    outerRadius={80}
+                    outerRadius={180}
                     fill="#8884d8"
                     dataKey="value"
                     label={({ name, percent }) => `${name} - ${(percent * 100).toFixed(0)}%`} >
